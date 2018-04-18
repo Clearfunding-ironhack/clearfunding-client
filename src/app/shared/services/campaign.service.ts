49b7fd1eef7435fca3cc2fa9abbdddbe4220cc98
Barbara Sanser <bsanser@gmail.com>
2018-04-18 21:06:57 +0200

    feat: minor fixes

--- src/app/shared/services/campaign.service.ts
+++ src/app/shared/services/campaign.service.ts
@@ -26,10 +26,9 @@ export class CampaignService extends BaseApiService {
         console.log("entro al res de getCampaign")
         res.json()}
       )
-      .catch(error =>{
-      console.log("entro al catch de getCampaign");
+      .catch(error => 
       this.handleError(error));
       }
   }
-}
+
 
